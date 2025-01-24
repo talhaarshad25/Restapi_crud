b0f6c70455081b5cc3f2ab641195cf5b96eaa9ba
Talha Arshad <officialtalhajutt25@gmail.com>
2025-01-24 18:11:45 +0500

    -initial start

--- app.js
+++ app.js
@@ -4,12 +4,13 @@ const express = require('express');
 const app = express();
 
 
+
 app.get('/api', (req, res) => {
   res.json({
       success:1, message: 'API is working'
   });
 });
 
-app.listen(3000, () => {
-  console.log('Server is running on port 3000');
+app.listen(process.env.APP_PORT, () => {
+  console.log('Server is running on port : ' + process.env.APP_PORT);
 });
